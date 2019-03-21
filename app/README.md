107fed1cca33468450c715e2c894e5dc58b4c9f7
Peter Törnhult <peter.tornhult@kindredgroup.com>
2019-03-21 09:05:57 +0100

    Initial import

---
+++ app/README.md
@@ -0,0 +1,4 @@
+# Testing Custom Views with Robolectric
+
+This project contains a simple custom text view. This will be tested using Robolectric and MockK for UI related functionality and Unit tested for anything not requiring a context nor UI related
+
---
+++ app/src/main/java/se/codeunlimited/testapplication/MainActivity.kt
@@ -0,0 +1,12 @@
+package se.codeunlimited.testapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
